c38e0d021b5e356fed60023c0aa635e187a8183a
gakuseh <eric@gakuseh.dev>
2026-03-19 15:24:24 -0400

    Add units to QR code width constant var name

--- include/event_handlers.hpp
+++ include/event_handlers.hpp
@@ -9,7 +9,7 @@
 #include "gtk_signal_data.hpp"
 #include "shared.hpp"
 
-const float QR_CODE_WIDTH = 6;
+const float QR_CODE_WIDTH_INCH = 6;
 
 namespace event_handlers {
     void on_calibrate_button_clicked (GtkWidget *widget, gpointer data);
